364e6fcb2a749fdbb2b271bc912529b5aee596c9
root <root@DESKTOP-KJNF2QE.>
2025-11-27 18:29:21 +0700

    refactor(..)

--- migration_preview.sql
+++
@@ -1,5 +0,0 @@
--- Add is_starred column to vocabulary table
-ALTER TABLE vocabulary ADD COLUMN is_starred BOOLEAN DEFAULT FALSE;
-
--- Add is_starred column to vocabulary_set table
-ALTER TABLE vocabulary_set ADD COLUMN is_starred BOOLEAN DEFAULT FALSE;
